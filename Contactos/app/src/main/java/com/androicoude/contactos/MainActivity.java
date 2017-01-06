4e78d2c2c6b0b0ccddffd53aeb02b15a9bcdb0b9
Gustavo Ovelar <gusove@gmail.com>
2017-01-06 19:20:55 -0200

    DatePicket, RaisedBooton
    Implementación de Calendario para fecha de nacimiento y Raised para
    boton Siguiente

--- Contactos/app/src/main/java/com/androicoude/contactos/MainActivity.java
+++ Contactos/app/src/main/java/com/androicoude/contactos/MainActivity.java
@@ -1,13 +1,46 @@
 package com.androicoude.contactos;
 
+import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Calendar;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageButton calendario;
+    EditText fecha;
+    private int dia, mes, ano;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendario = (ImageButton) findViewById(R.id.btn_setdate);
+        fecha = (EditText) findViewById(R.id.et_nacimiento);
+        calendario.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v==calendario)
+        {
+            final Calendar c = Calendar.getInstance();
+            dia=c.get(Calendar.DAY_OF_MONTH);
+            mes=c.get(Calendar.MONTH);
+            ano=c.get(Calendar.YEAR);
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    fecha.setText(dayOfMonth+"/"+(month+1)+"/"+year);
+                }
+            },dia,mes,ano);
+            datePickerDialog.show();
+        }
     }
 }
