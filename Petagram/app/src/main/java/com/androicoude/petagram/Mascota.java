0b14caa60f4e28060d822c8cb200c82b4fff4f4b
Gustavo Ovelar <gusove@gmail.com>
2017-01-21 16:29:26 -0200

    Implementación de Card View
    Implementación de Card View

--- Petagram/app/src/main/java/com/androicoude/petagram/Mascota.java
+++ Petagram/app/src/main/java/com/androicoude/petagram/Mascota.java
@@ -5,5 +5,37 @@ package com.androicoude.petagram;
  */
 
 public class Mascota {
-    
+    private int foto;
+    private String nombre;
+    private int raiting;
+
+    public Mascota(int foto,String nombre,int reiting){
+        this.foto=foto;
+        this.nombre=nombre;
+        this.raiting=reiting;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getRaiting() {
+        return raiting;
+    }
+
+    public void setRaiting(int raiting) {
+        this.raiting = raiting;
+    }
 }
---
+++ Petagram/app/src/main/java/com/androicoude/petagram/MascotaAdaptador.java
@@ -0,0 +1,19 @@
+package com.androicoude.petagram;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by a623214 on 21/01/2017.
+ */
+
+public class MascotaAdaptador {
+
+    public static class MascotaViewHolder extends RecyclerView.ViewHolder{
+
+
+        public MascotaViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
